c7b6ea3740f5618025938718ac5f989050ecfc68
Stefan Jovanovic <stefan.jovanavich@gmail.com>
2024-07-13 07:51:26 +0200

    - Updated

--- source-code/starter-project/ToDo-App-Compose-Multiplatform/composeApp/src/commonMain/kotlin/di/KoinModule.kt
+++ source-code/starter-project/ToDo-App-Compose-Multiplatform/composeApp/src/commonMain/kotlin/di/KoinModule.kt
@@ -4,13 +4,9 @@ import data.MongoDB
 import domain.MongoRepository
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
-import presentation.screen.home.HomeViewModel
-import presentation.screen.task.TaskViewModel
 
 val mongoModule = module {
     single<MongoRepository> { MongoDB() }
-    factory { HomeViewModel(mongoDB = get()) }
-    factory { TaskViewModel(mongoDB = get()) }
 }
 
 fun initializeKoin() {
